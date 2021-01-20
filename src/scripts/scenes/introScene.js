8907ebd26815df2ef6cef76581d7e339827aef50
Federico Portoghese <federico.portoghese@vimncontractor.com>
2021-01-20 17:47:57 +0100

    Fixes assets paths

--- src/scripts/scenes/introScene.js
+++ src/scripts/scenes/introScene.js
@@ -1,16 +1,16 @@
 import { HEIGHT, WIDTH, relativeWidth, relativeHeight } from '../config/global'
 
-import { FONT_STYLE } from '../config/game'
+import { FONT_STYLE, ASSETS } from '../config/game'
 
 export default class PreloadScene extends Phaser.Scene {
   constructor() {
     super({ key: 'PreloadScene' })
   }
   preload() {
-    this.load.image('button', './src/assets/img/button.png')
-    this.load.image('bullet', './src/assets/img/bullet.png')
-    this.load.image('ship', './src/assets/img/ship.png')
-    this.load.image('sky', './src/assets/img/bg.jpg')
+    this.load.image('button', '../../assets/img/button.png')
+    this.load.image('bullet', '../../assets/img/bullet.png')
+    this.load.image('ship', '../../assets/img/ship.png')
+    this.load.image('sky', '../../assets/img/bg.jpg')
   }
   create() {
     this.background = this.add.tileSprite(relativeWidth(50), relativeHeight(50), WIDTH, HEIGHT, 'sky')
